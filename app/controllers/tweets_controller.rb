f237f87dad72b62c5187b830645c242e393f4874
a1851yl <a1851yl@aiit.ac.jp>
2018-07-26 05:08:02 +0000

    remove beforeaction

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user
+  # before_action :logged_in_user
   # GET /tweets
   # GET /tweets.json
   def index
