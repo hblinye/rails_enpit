ef258a2a2d24f11a2fe8969347a833cb5105867d
a1851yl <a1851yl@aiit.ac.jp>
2018-07-27 10:35:23 +0000

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180725094934) do
+ActiveRecord::Schema.define(version: 20180727102401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180725094934) do
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "birthplace"
   end
 
 end
